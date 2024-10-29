9a9f94d266f7b2a902cbfaec9c8b940f8cd05950
Alef Basilio <alefsilva0505@gmail.com>
2024-10-29 18:06:30 -0300

    addAllCollectionItems image path parameter adjust

--- javascript/addAllCollectionItems.js
+++ javascript/addAllCollectionItems.js
@@ -1,21 +1,21 @@
 import { collectionItem } from "./collectionItem.js";
 
-collectionItem("/slasla/images/collectionItem1.jfif", "01/01/0001", "Cycling", 
+collectionItem("../images/collectionItem1.jfif", "01/01/0001", "Cycling", 
     "Ciclism, or cycling, is an excellent way to improve your physical and mental health while having fun. Regular cycling can help you build cardiovascular endurance, increase muscle strength and flexibility, and boost your mood. It's also an environmentally friendly mode of transportation and a great way to explore new places."
 );
 
-collectionItem("/slasla/images/collectionItem2.jpg", "01/01/0001", "Hike", 
+collectionItem("../images/collectionItem2.jpg", "01/01/0001", "Hike", 
     "Hiking is a wonderful way to connect with nature and improve your overall well-being. Being outdoors and surrounded by natural beauty can help reduce stress and anxiety, while the physical activity of hiking can boost your mood and energy levels. Hiking also provides an excellent workout, improving cardiovascular health, strengthening muscles, and increasing flexibility."
 );
 
-collectionItem("/slasla/images/collectionItem3.jpg", "01/01/0001", "Climbing", 
+collectionItem("../images/collectionItem3.jpg", "01/01/0001", "Climbing", 
     "Climbing is a challenging and rewarding activity that offers numerous physical and mental benefits. It requires strength, endurance, and mental focus, making it an excellent way to improve overall fitness. It can also help you develop problem-solving skills, build confidence, and improve your coordination and balance."
 );
 
-collectionItem("/slasla/images/collectionItem4.jfif", "01/01/0001", "Waterfall", 
+collectionItem("../images/collectionItem4.jfif", "01/01/0001", "Waterfall", 
     "Taking a shower in a waterfall is a truly invigorating experience that can leave you feeling refreshed, revitalized, and connected to nature. The force of the water can be incredibly therapeutic, massaging away tension and stress, while the cool water can help to reduce inflammation and improve circulation."
 );
 
-collectionItem("/slasla/images/collectionItem5.jpg", "01/01/0001", "Beach", 
+collectionItem("../images/collectionItem5.jpg", "01/01/0001", "Beach", 
     "Taking a shower in a beach setting can be a truly rejuvenating experience. The warmth of the sun and the soothing sound of the waves can help to calm and relax the mind, while the cool water can provide a refreshing sensation on the skin. Being surrounded by the beauty of nature can also have a profound impact on mental well-being, promoting a sense of peace and tranquility."
 );
